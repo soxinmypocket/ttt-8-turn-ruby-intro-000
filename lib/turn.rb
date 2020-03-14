fffa1e75ba6c9be029a4c15984c9f47e878b8cea
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-14 10:03:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,14 +8,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#coverts a user's place on the board to the index integer
 def input_to_index(user_input)
   index = user_input.to_i
   index -= 1
   return index
 end
 
-#checks the number to see if 1) the position is already taken and 2)it is an actual number
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
@@ -40,13 +38,12 @@ def valid_move?(board, index)
   end
 end
 
-#Takes a users num and the board, and places the X or O char in that position
+
 def move(board, index, character = "X")
   board[index] = character
   return board
 end
 
-#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
 def turn (board)
   puts "Please enter 1-9:"
   num = gets.chomp
